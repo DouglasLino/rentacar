b13f9f3e137a28fa157db7a0533d5c734a07f1b8
Christopher Olmedo <christov.es@hotmail.com>
2021-02-18 21:14:41 -0600

    Creación de Models de tablas

---
+++ renta_autos/renta_autos/Models/Alquiler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace renta_autos.Models
+{
+    public class Alquiler
+    {
+        //Atributos / campos de clase y tabla
+        public int Id_alquiler { get; set; }
+
+        [Required(ErrorMessage = "El cliente es requerido")]
+        public string Cliente { get; set; }
+
+        [Required(ErrorMessage = "El telefono del cliente es requerido")]
+        [Range(9, 9, ErrorMessage = "Ingrese número de telefono válido")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El telefono del cliente es requerido")]
+        [Range(10, 10, ErrorMessage = "Ingrese número de DUI válido")]
+        public string Dui { get; set; }
+
+        [Required(ErrorMessage = "La fecha de inicio es requerida")]
+        public DateTime Fecha_inicio { get; set; }
+
+        [Required(ErrorMessage = "La fecha de fin es requerida")]
+        public DateTime Fecha_fin { get; set; }
+
+        [Required]
+        public double Total { get; set; }
+
+        //Llaves foraneas de la tabla vehiculo
+        public int Id_vehiculo { get; set; }
+
+        //Codificación para mostrar atributos por medio de los Id
+        //Esto lo haré según decidamos los detalles que mostraremos en las vistas 
+    }
+}
---
+++ renta_autos/renta_autos/Models/Vehiculos.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace renta_autos.Models
+{
+    public class Vehiculos
+    {
+        //Atributos / campos de clase y tabla
+        [Key]
+        public int Id_vehiculo { get; set; }
+
+        [Required(ErrorMessage = "La placa es requerida")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "El año es requerido")]
+        [Range(2000, 2022, ErrorMessage = "Ingrese año válido del vehículo")]
+        public int Anio { get; set; }
+
+        [Required(ErrorMessage = "El tipo de transmisión es requerido")]
+        public string Transmision { get; set; }
+
+        [Required(ErrorMessage = "El precio es requerido")]
+        [Range(1, 10, ErrorMessage = "Ingrese precio de la hora válido")]
+        public double Precio_hora { get; set; }
+
+        [Required]
+        public bool Estado { get; set; }
+
+        //Llave foranea para la tabla de Alquiler
+        [ForeignKey("Id_vehiculo")]
+        public ICollection<Alquiler> Alquileres{ get; set; }
+
+        //Llaves foraneas de las tablas catálogos
+        public int Id_marca { get; set; }
+        public int Id_modelo { get; set; }
+        public int Id_tipo_vehiculo { get; set; }
+
+        //Codificación para mostrar los nombres de las tablas catálogos en vez de los Id
+        [NotMapped]
+        public string Marca
+        {
+            get
+            {
+                using (var context = new Contexto())
+                {
+                    var Marca = context.Marcas.Where(x => x.Id_marca == Id_marca).Select(x => x.Marca).FirstOrDefault();
+                    return Marca;
+                }
+            }
+        }
+        [NotMapped]
+        public string Modelo
+        {
+            get
+            {
+                using (var context = new Contexto())
+                {
+                    var Modelo = context.Modelos.Where(x => x.Id_modelo == Id_modelo).Select(x => x.Modelo).FirstOrDefault();
+                    return Modelo;
+                }
+            }
+        }
+        [NotMapped]
+        public string Tipo_vehiculo
+        {
+            get
+            {
+                using (var context = new Contexto())
+                {
+                    var Tipo_vehiculo = context.TipoVehiculos.Where(x => x.Id_tipo_vehiculo == Id_tipo_vehiculo).Select(x => x.Tipo_vehiculo).FirstOrDefault();
+                    return Tipo_vehiculo;
+                }
+            }
+        }
+    }
+}
