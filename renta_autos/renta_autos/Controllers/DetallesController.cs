cf3dae394010e3e0c1691c068544459f26dc6380
tejadakatherine19 <tejadakatherine844@gmail.com>
2021-02-23 01:32:42 -0600

    DetalleAlquiler

---
+++ renta_autos/renta_autos/Controllers/DetallesController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using renta_autos.Models;
+using Rotativa;
+
+namespace renta_autos.Controllers
+{
+    public class DetallesController : Controller
+    {
+        // GET: Detalles
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult CreateUpdate(int Id_alquiler = 0)
+        {
+            if (Id_alquiler > 0)
+            {
+                using (var context = new Contexto())
+                {
+                    var data = context.Alquileres.Where(x => x.Id_alquiler == Id_alquiler).FirstOrDefault();
+                    return View(data);
+
+                }
+            }
+            else
+            {
+                Alquiler alquiler = new Alquiler();
+                return View(alquiler);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateUpdate(Alquiler alquiler)
+        {
+            if (ModelState.IsValid)
+            {
+                //alquiler.Total = ()
+
+                var Nuevo = alquiler.Id_alquiler == 0 ? true : false;
+
+                if (Nuevo)
+                {
+                    using (var context = new Contexto())
+                    {
+                        context.Alquileres.Add(alquiler);
+                        context.SaveChanges();
+                    }
+                }
+                else
+                {
+                    using (var context = new Contexto())
+                    {
+                        var data = context.Alquileres.Where(x => x.Id_alquiler == alquiler.Id_alquiler).FirstOrDefault();
+                        data.Cliente = alquiler.Cliente;
+                        data.Telefono = alquiler.Telefono;
+                        data.Dui = alquiler.Dui;
+                        data.Fecha_inicio = alquiler.Fecha_inicio;
+                        data.Fecha_fin = alquiler.Fecha_fin;
+                        data.Total = alquiler.Total;
+
+
+                        context.SaveChanges();
+                    }
+
+                }
+
+                return View("Enviados");
+            }
+            else
+            {
+                return View(alquiler);
+            }
+        }
+
+
+        public ActionResult MostrarDetalle()
+        {
+            using (var context = new Contexto())
+            {
+                var data = context.Alquileres.ToList();
+                ViewBag.datos = data;
+
+                return View();
+            }
+        }
+
+        public ActionResult Imprimir()
+        {
+            using (var context = new Contexto())
+            {
+                var data = context.Alquileres.ToList();
+                ViewBag.datos = data;
+            }
+
+            return new ViewAsPdf("PDF")
+            {
+                PageSize = Rotativa.Options.Size.A4,
+                PageMargins = new Rotativa.Options.Margins(10, 20, 10, 20),
+                FileName = "Factura.pdf"
+            };
+        }
+    }
+
+}
